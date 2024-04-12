dd8e02b88c934aa60eaa6c5b88ae61d470e089bf
Natsumi Taniuchi <nat.taniuchi@gmail.com>
2024-04-12 05:52:32 -0500

    add header files

--- TrackRebuilder.h
+++ TrackRebuilder.h
@@ -1,6 +1,21 @@
 #include "TrackHitCollector.h"
 #include "TrackRebuilder.h"
 
+#include "art/Framework/Core/EDProducer.h"
+#include "lardataobj/RecoBase/TrackHitMeta.h"
+#include "art/Persistency/Common/PtrMaker.h"
+
+#include "larpandoracontent/LArHelpers/LArPfoHelper.h"
+#include "larpandoracontent/LArObjects/LArPfoObjects.h"
+
+#include "larpandora/LArPandoraInterface/LArPandoraHelper.h"
+#include "larpandora/LArPandoraInterface/LArPandoraOutput.h" 
+
+#include <iostream>
+
+#ifndef TRACK_REBUILDER
+#define TRACK_REBUILDER 1
+
 using namespace pandora;
 
 namespace kaon_reconstruction 
@@ -96,3 +111,5 @@ recob::Track TrackRebuilder::build_track(int track_id, lar_content::LArTrackStat
 }
 
 }//namespace kaon_reconstruction
+
+#endif
